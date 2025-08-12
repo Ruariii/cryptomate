d1ea4ea5b57fb8406bd9ebf1c41a325143f63593
Ruari Hodgin <ruarihodgin99@outlook.com>
2025-08-12 17:01:06 +0100

    ema ans price data

--- backend/app/polygon_client.py
+++ backend/app/polygon_client.py
@@ -5,6 +5,7 @@ import requests
 from dotenv import load_dotenv
 from polygon import RESTClient
 from datetime import datetime
+from datetime import timedelta
 
 load_dotenv()
 API_KEY = os.getenv("POLYGON_API_KEY")
@@ -12,24 +13,39 @@ client = RESTClient(API_KEY)
     
 
 
-def get_indicator_data(symbol: str, timespan="hour"):
+def get_indicator_data(symbol: str, timespan="minute"):
 
     ticker = f'X:{symbol.upper()}USD'
 
     # Fetch indicators
-    rsi_data = client.get_rsi(ticker=ticker, timespan=timespan, window=14, series_type="close", order="desc", limit=1)
+    ema_data = client.get_ema(ticker=ticker, timespan=timespan, window=20, series_type="close", order="desc", limit=1)
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
 
+    snapshot_data = []
+    for a in client.list_aggs(
+        ticker,
+        1,
+        "minute",
+        yesterday,
+        today,
+        adjusted="true",
+        sort="desc",
+        limit=2,
+    ):
+        snapshot_data.append(a)
 
     # Extract latest values
-    rsi_value = rsi_data.values[0].value
-
-
-
+    ema_value = ema_data.values[0].value
+    latest_price = snapshot_data[0].results[0].c
+    
+    
 
 
 
     # Return structured result
     return {
         "ticker": ticker,
-        "rsi": rsi_value
+        "ema": ema_value,
+        "price": latest_price
     }
